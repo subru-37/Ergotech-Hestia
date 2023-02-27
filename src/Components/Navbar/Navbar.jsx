74bae5be6d6564e79fcc526ca3897a1813cb797f
subru-37 <easwaransubramaniOO7@gmail.com>
2023-02-27 21:21:15 +0530

    navbar is fully functional design wise

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
-
+import React,{useState,useEffect} from 'react'
+import './Navbar.css'
 export default function Navbar() {
-  return (
-    <div>
+  const [scroll, setScroll] = useState(false);
+  const [scrollp, setScrollP] = useState(window.scrollY);
+  useEffect(() => {
+    window.addEventListener("scroll", navScrolled);
+    function navScrolled(e) {
+      setScrollP(window.scrollY);
+      if (window.scrollY > scrollp) {
+        setScroll(true);
+        console.log(scrollp,scroll)
+      } else if (window.scrollY <= scrollp){
+        setScroll(false);
+      }
+      // console.log(scrollp, scroll)
+    }
 
+    return () => {
+      window.removeEventListener("scroll", navScrolled);
+    };
+  }, [scrollp]);
+  return (
+    <div className={ scroll ? 'parent_navbar scrolled' : 'parent_navbar'}>
+      <div className='container'>
+      <div className='logo'>
+            <h2>Hestia</h2>
+        </div>
+        <div>
+          <ul className='navitems' style={{listStyle:'none',display:'flex',flexDirection:'row'}}>
+            <li className='navitem'>Home</li>
+            <li className='navitem'>Search</li>
+            <li className='navitem'>About Us</li>
+          </ul>
+        </div>
+        <div className='buttons'>
+          <button className='signup'>Sign up</button>
+          <button className='signin'>Sign in</button>
+        </div>
+      </div>
     </div>
   )
 }
--- src/Pages/Landing/Landing.jsx
+++ src/Pages/Landing/Landing.jsx
@@ -3,7 +3,7 @@ import './Landing.css'
 import Navbar from '../../Components/Navbar/Navbar'
 export default function Landing() {
   return (
-    <div>
+    <div style={{minHeight:'200vh'}}>
         <Navbar/>
     </div>
   )
