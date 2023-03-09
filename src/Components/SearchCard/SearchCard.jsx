af5c270b7d2bec97ae82489bbac1f0e5b8b85c4a
subru-37 <easwaransubramaniOO7@gmail.com>
2023-03-09 10:45:16 +0530

    removed warnings

--- src/Components/SearchCard/SearchCard.jsx
+++ src/Components/SearchCard/SearchCard.jsx
@@ -4,7 +4,7 @@ import imtg from '../../Assets/img-pg.png'
 export default function SearchCard(props) {
   return (
     <div className='card-section'>
-        <div className='pic-section'><img className='Image' src={imtg}></img></div>
+        <div className='pic-section'><img className='Image' alt='' src={imtg}></img></div>
         <div className='preview-section'>
             <div className='preview-title'><p className='title'>{props.title}</p><p style={{fontSize:'1rem'}}>{props.mw}</p></div>
             <p style={{fontWeight: '400',fontSize: '1rem',color: '#2C2A2A',paddingBottom:'10px',lineHeight: '15px'}}>Description</p>
