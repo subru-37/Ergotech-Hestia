ef83581eb7fb3aa355cb74e9409a1fff11c3a26f
Alaka A J <alaka03aj@gmail.com>
2023-03-09 21:31:36 +0530

    landing page added

---
+++ src/Components/TopPicks/TopPicks.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./TopPicks.css";
+import top from "../../Assets/top-picks-img.svg";
+
+const TopPicks = (props) => {
+  return (
+    <div className="main-container">
+      <div className="image-container">
+        <div className="image">
+          <img src={top}></img>
+        </div>
+      </div>
+      <h3>{props.title}</h3>
+      <p>{props.desc}</p>
+    </div>
+  );
+};
+
+export default TopPicks;
