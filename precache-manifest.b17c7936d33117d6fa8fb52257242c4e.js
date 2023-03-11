e3b45a95c1bb5766084f28b01331eaa05468227d
subru-37 <easwaransubramaniOO7@gmail.com>
2023-03-12 04:23:39 +0530

    Updates

---
+++ precache-manifest.b17c7936d33117d6fa8fb52257242c4e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "4cd1c4f3a940acae9fec",
+    "url": "/Ergotech-Hestia/static/css/main.aeb9c212.chunk.css"
+  },
+  {
+    "revision": "4cd1c4f3a940acae9fec",
+    "url": "/Ergotech-Hestia/static/js/main.9c1475bc.chunk.js"
+  },
+  {
+    "revision": "1c667200eb49187f3829",
+    "url": "/Ergotech-Hestia/static/js/runtime~main.7aabd0ef.js"
+  },
+  {
+    "revision": "a13ac5f54576e3d7d617",
+    "url": "/Ergotech-Hestia/static/js/2.1b448332.chunk.js"
+  },
+  {
+    "revision": "c2d2527b54fa17a65902",
+    "url": "/Ergotech-Hestia/static/js/3.1dc37123.chunk.js"
+  },
+  {
+    "revision": "8dcd494556f4be22bfe6fde07628fb1d",
+    "url": "/Ergotech-Hestia/static/media/apartment.8dcd4945.svg"
+  },
+  {
+    "revision": "89c53d35e51188668a1da0f275f04163",
+    "url": "/Ergotech-Hestia/static/media/about-team-image.89c53d35.svg"
+  },
+  {
+    "revision": "e4857829cd9c5feed2fd73ad2158112d",
+    "url": "/Ergotech-Hestia/static/media/City.e4857829.png"
+  },
+  {
+    "revision": "d50caca3e0bcc1e2140cc0d9933eab92",
+    "url": "/Ergotech-Hestia/static/media/IMG20221025172200.d50caca3.jpg"
+  },
+  {
+    "revision": "81df4024f3f0990f25c6729253f9b048",
+    "url": "/Ergotech-Hestia/index.html"
+  }
+];
