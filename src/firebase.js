b82ac1dc93fd9af784827428fae73a54d01d2a9f
subru-37 <easwaransubramaniOO7@gmail.com>
2023-03-16 12:19:48 +0530

    tried fixing auth issue

--- src/firebase.js
+++ src/firebase.js
@@ -6,10 +6,10 @@ import { getAuth} from "firebase/auth";
 
 
 const firebaseConfig = {
-        apiKey: 'IzaSyDNbfqE8F9d6pJCrwWQrMi9QX7hOLn8e18',
+        apiKey: "IzaSyDNbfqE8F9d6pJCrwWQrMi9QX7hOLn8e18",
          authDomain: "hestia-73c85.firebaseapp.com",
-        projectId: 'hestia-73c85',
-        storageBucket:  'hestia-73c85.appspot.com',
+        projectId: "hestia-73c85",
+        storageBucket:  "hestia-73c85.appspot.com",
         messagingSenderId: "662574977698",
         appId: "1:662574977698:web:9d8de00e9f1a7a5a823dfb",
         measurementId: "G-EY3FGFZSX4" 
