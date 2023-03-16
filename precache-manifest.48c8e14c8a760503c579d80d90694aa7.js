f9c00c4789625ed2fd2df61c0d6efcf92ea8b132
subru-37 <easwaransubramaniOO7@gmail.com>
2023-03-16 12:14:10 +0530

    Updates

--- precache-manifest.48c8e14c8a760503c579d80d90694aa7.js
+++ precache-manifest.48c8e14c8a760503c579d80d90694aa7.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "b37ce280b73be02fa754",
+    "revision": "7978863055dbcc61675a",
     "url": "/Ergotech-Hestia/static/css/main.aeb9c212.chunk.css"
   },
   {
-    "revision": "b37ce280b73be02fa754",
-    "url": "/Ergotech-Hestia/static/js/main.61a3a1f9.chunk.js"
+    "revision": "7978863055dbcc61675a",
+    "url": "/Ergotech-Hestia/static/js/main.d8f785f2.chunk.js"
   },
   {
     "revision": "bb3c2f4f42db21962611",
@@ -36,7 +36,7 @@ self.__precacheManifest = [
     "url": "/Ergotech-Hestia/static/media/IMG20221025172200.d50caca3.jpg"
   },
   {
-    "revision": "14d7570ac612dea485cf880d35b89239",
+    "revision": "d182bc3bc387f61d0d9d61d816d30934",
     "url": "/Ergotech-Hestia/index.html"
   }
 ];
